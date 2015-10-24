45171a8672eeb0e2578972cc33c27a337804e287
Alleo Indong <freedom_war550@live.com.ph>
2015-10-25 02:17:54 +0800

    updated login design activity

--- app/src/main/java/com/aindong/restoko/MainActivity.java
+++ app/src/main/java/com/aindong/restoko/MainActivity.java
@@ -1,13 +1,17 @@
 package com.aindong.restoko;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String accessToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
