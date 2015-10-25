b42496f3f81cae5d577d6bac0e448f209cfab12d
Alleo Indong <freedom_war550@live.com.ph>
2015-10-25 09:40:59 +0800

    added comments

--- app/src/main/java/com/aindong/restoko/LoginActivity.java
+++ app/src/main/java/com/aindong/restoko/LoginActivity.java
@@ -17,31 +17,46 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText textUsername = (EditText) findViewById(R.id.textUsername);
-        EditText textPassword = (EditText) findViewById(R.id.textPassword);
+        // Get the reference of controls to be able to access their values
+        final EditText textUsername = (EditText) findViewById(R.id.textUsername);
+        final EditText textPassword = (EditText) findViewById(R.id.textPassword);
         Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Authenticating please wait...", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-
-                Thread thread = new Thread() {
-                    @Override
-                    public void run() {
-                        try {
-                            Thread.sleep(2000);
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-
-                thread.start();
+                doLogin(textUsername, textPassword);
             }
         });
+    }
+
+    private void doLogin(final EditText username, final EditText password) {
+        // Show a toast message for feedback purposes of button click
+        Toast toast = Toast.makeText(getApplicationContext(), "Authenticating please wait...", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        // Create a new thread for processing the login
+        // This is needed to not disrupt the main thread or ui thread from hanging
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+
+                    // Create a new intent for showing main activity
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("username", username.getText());
+                    intent.putExtra("password", password.getText());
+
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        // Start the thread worker
+        thread.start();
     }
 }
