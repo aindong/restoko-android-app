846e6466cce656fa50bb94eb1b716972acab9d74
Alleo Indong <freedom_war550@live.com.ph>
2015-10-25 09:36:30 +0800

    added dummy login action

--- app/src/main/java/com/aindong/restoko/LoginActivity.java
+++ app/src/main/java/com/aindong/restoko/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.aindong.restoko;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
@@ -11,5 +17,31 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        EditText textUsername = (EditText) findViewById(R.id.textUsername);
+        EditText textPassword = (EditText) findViewById(R.id.textPassword);
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Authenticating please wait...", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+
+                Thread thread = new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(2000);
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                thread.start();
+            }
+        });
     }
 }
