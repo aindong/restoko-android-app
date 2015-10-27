671cbaeb3eb51982e3fae328f0f958cb683c4d74
Alleo Indong <freedom_war550@live.com.ph>
2015-10-27 14:23:09 +0800

    updated product, added properties

--- app/src/main/java/com/aindong/restoko/models/Product.java
+++ app/src/main/java/com/aindong/restoko/models/Product.java
@@ -4,7 +4,9 @@ public class Product {
     public int id;
     public String name;
     public String slug;
+    public String description;
     public String image_url;
+    public double amount;
     public int category_id;
 
     /**
@@ -13,13 +15,21 @@ public class Product {
      * @param id
      * @param name
      * @param slug
+     * @param description
      * @param image_url
+     * @param amount
      * @param category_id
      */
-    public Product(int id, String name, String slug, String image_url, int category_id) {
+    public Product(int id, String name, String slug, String description, String image_url,
+                   double amount, int category_id) {
+
+        // Assign parameters
         this.id = id;
         this.name = name;
         this.slug = slug;
+        this.description = description;
+        this.image_url = image_url;
+        this.amount = amount;
         this.category_id = category_id;
     }
 }
