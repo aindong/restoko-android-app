652d0c681b89da2b360ce4b0331ef132663d418f
Alleo Indong <freedom_war550@live.com.ph>
2015-10-27 13:41:35 +0800

    updated category and product model object

--- app/src/main/java/com/aindong/restoko/models/Category.java
+++ app/src/main/java/com/aindong/restoko/models/Category.java
@@ -1,5 +1,53 @@
 package com.aindong.restoko.models;
 
-public class Category {
+import java.util.List;
 
+public class Category {
+    // Id property of the category
+    public int id;
+
+    // Name property of the category
+    public String name;
+
+    // Slug property of the category
+    public String slug;
+
+    // List of Products inside the category
+    public List<Product> products;
+
+    /**
+     * Constructor when creating a new category
+     *
+     * @param id
+     * @param name
+     * @param slug
+     */
+    public Category(int id, String name, String slug) {
+        this.id = id;
+        this.name = name;
+        this.slug = slug;
+    }
+
+    /**
+     * Set the products of this category
+     *
+     * @param products
+     */
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    /**
+     * Add product to list
+     *
+     * @param product
+     */
+    public void addProduct(Product product) {
+        // This product does not belong here
+        if (product.category_id != this.id) {
+            return;
+        }
+
+        this.products.add(product);
+    }
 }
--- app/src/main/java/com/aindong/restoko/models/Product.java
+++ app/src/main/java/com/aindong/restoko/models/Product.java
@@ -1,5 +1,23 @@
 package com.aindong.restoko.models;
 
 public class Product {
+    public int id;
+    public String name;
+    public String slug;
+    public int category_id;
 
+    /**
+     * Constructor when creating a new product object
+     *
+     * @param id
+     * @param name
+     * @param slug
+     * @param category_id
+     */
+    public Product(int id, String name, String slug, int category_id) {
+        this.id = id;
+        this.name = name;
+        this.slug = slug;
+        this.category_id = category_id;
+    }
 }
