ceba8f32fddc93f5fbeb33351d8ccf1105ebd3db
Alleo Indong <freedom_war550@live.com.ph>
2015-10-27 13:42:53 +0800

    updated product model to add image

--- app/src/main/java/com/aindong/restoko/models/Product.java
+++ app/src/main/java/com/aindong/restoko/models/Product.java
@@ -4,6 +4,7 @@ public class Product {
     public int id;
     public String name;
     public String slug;
+    public String image_url;
     public int category_id;
 
     /**
@@ -12,9 +13,10 @@ public class Product {
      * @param id
      * @param name
      * @param slug
+     * @param image_url
      * @param category_id
      */
-    public Product(int id, String name, String slug, int category_id) {
+    public Product(int id, String name, String slug, String image_url, int category_id) {
         this.id = id;
         this.name = name;
         this.slug = slug;
