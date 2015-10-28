39cc51e69cf8674e73e74c2e13cb7ed8c748549d
Alleo Indong <freedom_war550@live.com.ph>
2015-10-28 15:49:51 +0800

    created cartItem model

---
+++ app/src/main/java/com/aindong/restoko/models/CartItem.java
@@ -0,0 +1,25 @@
+package com.aindong.restoko.models;
+
+public class CartItem {
+    public int id;
+    public Product product;
+    public int quantity;
+
+    public CartItem(int id, Product product, int quantity) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public void addQuantity(int qty) {
+        this.quantity += qty;
+    }
+
+    public void subtractQuantity(int qty) {
+        if (this.quantity <= 1) {
+            return;
+        }
+
+        this.quantity -= qty;
+    }
+}
