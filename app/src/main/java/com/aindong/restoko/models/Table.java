e42479c0ade82149526d331e9e941287296bfa2d
Alleo Indong <freedom_war550@live.com.ph>
2015-10-28 11:22:34 +0800

    created table model

---
+++ app/src/main/java/com/aindong/restoko/models/Table.java
@@ -0,0 +1,13 @@
+package com.aindong.restoko.models;
+
+public class Table {
+    public int id;
+    public String name;
+    public String status;
+
+    public Table(int id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+}
