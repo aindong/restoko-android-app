6fd8f2902cd6e5ca4a887bc33d284173fd367a3a
Alleo Indong <freedom_war550@live.com.ph>
2015-10-28 09:54:49 +0800

    updated product pictures

--- app/src/main/java/com/aindong/restoko/ProductMenuFragment.java
+++ app/src/main/java/com/aindong/restoko/ProductMenuFragment.java
@@ -105,14 +105,14 @@ public class ProductMenuFragment extends Fragment {
         appetizers.addProduct(new Product(1, "Nachos Deluxe",
                 "best-ever-jalapeno-poppers",
                 "Crispy nacho chips topped with taco beef, jalapeno, salsa, olives and cheese",
-                "http://i.imgur.com/a1GwO8A.jpg",
+                "https://i.imgur.com/rjoLxVH.jpg",
                 145,
                 appetizers.id));
 
         appetizers.addProduct(new Product(1, "Java Chicken Wings",
                 "best-ever-jalapeno-poppers",
                 "Deep fried 3 pieces chicken wings coated with BBQ sauce served with crudités in Java aioli",
-                "http://i.imgur.com/a1GwO8A.jpg",
+                "https://i.imgur.com/l2VyrBL.jpg",
                 140,
                 appetizers.id));
 
@@ -131,7 +131,49 @@ public class ProductMenuFragment extends Fragment {
                 appetizers.id));
 
 
+        // CHEFS RECOMMENDATIONS
+        recommendations.addProduct(new Product(1, "Surf and Turf",
+                "surf-and-turf",
+                "Grilled “Angus” Salisbury steak and breaded shrimp served with Java gravy, green salad and rice.",
+                "https://i.imgur.com/Blt6mYj.jpg",
+                280,
+                recommendations.id));
 
+        recommendations.addProduct(new Product(1, "Grilled Pork Steak",
+                "surf-and-turf",
+                "Marinated pork steak grilled to perfection served with buttered vegetables, rice and Java gravy",
+                "https://i.imgur.com/WF65HQw.png",
+                280,
+                recommendations.id));
+
+        recommendations.addProduct(new Product(1, "Herb and Nut Crusted Fish Fillet",
+                "surf-and-turf",
+                "Crispy pan-fried fish fillet coated with herbs and nuts served with green salad and rice",
+                "https://i.imgur.com/WF65HQw.png",
+                190,
+                recommendations.id));
+
+        // PIZZA AND SALADS
+        pizzas.addProduct(new Product(1, "Java Garden Salad",
+                "surf-and-turf",
+                "Lettuce, tomato, turnips, carrots, cucumber, bell peppers served with honey orange vinaigrette.",
+                "https://i.imgur.com/T8K88Yq.jpg",
+                115,
+                pizzas.id));
+
+        pizzas.addProduct(new Product(1, "Pizza Margherita",
+                "surf-and-turf",
+                "Homemade pizza with Java tomato sauce.",
+                "https://i.imgur.com/NQeYwNu.jpg",
+                155,
+                pizzas.id));
+
+        pizzas.addProduct(new Product(1, "Java Caesar Salad",
+                "surf-and-turf",
+                "Our version of Caesar salad, lettuce, bacon, croutons, parmesan cheese served with homemade Caesar dressing",
+                "https://i.imgur.com/jZhvUwy.jpg",
+                155,
+                pizzas.id));
 
 
 
